47e03026e5304b0e3817cfdbb448ba83f9ee9292
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-09-16 13:55:29 -0500

    Changed g.drawRect() to g.draw(new Rectangle())
    The Rectangle class allows us to get attributes unlike the .drawRect()

--- Main.java
+++ Main.java
@@ -9,16 +9,15 @@ public class Main extends JPanel implements ActionListener {
 	// The frame dimensions
 	static int frameWidth = 600;
 	static int frameHeight = 600;
-    
-    // The robot’s rotation
-    static int rotation = 0;
-    
-    // The robot's cordinates, use these to move it/turn it
-    static int robotX = 0;
-    static int robotY = 0;
-	
-	// Create a timer for the drawing update delay (originally "5, this")
-	Timer timer = new Timer(1, this);
+
+	// The robot’s rotation
+	static int rotation = 0;
+
+	static int robotX = 0;
+	static int robotY = 0;
+
+	// Create a timer for the refresh delay
+	static Timer timer = new Timer(5, this);
 	
 	public Main() {
 		timer.start();
@@ -27,46 +26,50 @@ public class Main extends JPanel implements ActionListener {
 	// Use this to start drawing all the lines
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-        g.setColor(Color.blue);
+		g.setColor(Color.blue);
         
-        Graphics2D g2 = (Graphics2D)g;
+		Graphics2D g2 = (Graphics2D)g;
         
-        // Change the origin point
+		// Change the origin point
 		g2.translate(this.getWidth()/3 + 100, this.getHeight()/2); //105
 		
 		// rotate the graphics board
 		//System.out.println(Math.toRadians(rotation)+" "+rotation%360);
 		g2.rotate(Math.toRadians(rotation));
 		
+		// Draw the robot
 		newMeccanumBot(g2);
 	}
 	
 	// Draw the Meccanum drivetrain
 	public void newMeccanumBot(Graphics2D g) {
 		// Proper use: X, Y, Width, Height
-		int[] chassis  = {-75, -100, 150, 200}; //{-75, -100, 150, 200}
-		int[] upperRightWheel = {75, -100, 25, 60}; //{75, -100, 25, 60}
-		int[] lowerRightWheel = {75, 40, 25, 60}; //{75, 40, 25, 60}
-		int[] upperLeftWheel = {-100, -100, 25, 60}; //{-100, -100, 25, 60}
-		int[] lowerLeftWheel = {-100, 40, 25, 60}; //{-100, 40, 25, 60}
-		newRect(g, chassis);
-		newRect(g, upperRightWheel);
-		newRect(g, lowerRightWheel);
-		newRect(g, upperLeftWheel);
-        newRect(g, lowerLeftWheel);
+		int[] chassis_cords  = {-75, -100, 150, 200}; //{-75, -100, 150, 200}
+		int[] upperRightWheel_cords = {75, -100, 25, 60}; //{75, -100, 25, 60}
+		int[] lowerRightWheel_cords = {75, 40, 25, 60}; //{75, 40, 25, 60}
+		int[] upperLeftWheel_cords = {-100, -100, 25, 60}; //{-100, -100, 25, 60}
+		int[] lowerLeftWheel_cords = {-100, 40, 25, 60}; //{-100, 40, 25, 60}
+		
+		// We use “Rectangle” because we can get attributes from that
+		Rectangle chassis = newRect(g, chassis_cords);
+		Rectangle upperRightWheel = newRect(g, upperRightWheel_cords);
+		Rectangle lowerRightWheel = newRect(g, lowerRightWheel_cords);
+		Rectangle upperLeftWheel = newRect(g, upperLeftWheel_cords);
+		Rectangle lowerLeftWheel = newRect(g, lowerLeftWheel_cords);
         
-        robotX = chassis[0];
-        robotY = chassis[1];
+		// Set the local X and Y attributes
+		robotX = (int)chassis.getX();
+		robotY = (int)chassis.getY();
         
-        // Draw the center point
-        g.drawOval(0, 0, 3, 3);
+		// Draw the center Point
+		g.drawOval(0, 0, 3, 3);
         
-        // To get rid of ...not now
-        //int[] chassis  = {this.getWidth()/3 + 25, this.getHeight()/2 + 5, 150, 200}; // X Y Width Height
-        //int[] upperRightWheel = {this.getWidth()/3 + 175, this.getHeight()/2 + 5, 25, 60};
-        //int[] lowerRightWheel = {this.getWidth()/3 + 175, this.getHeight()/2 + 145, 25, 60};
-        //int[] upperLeftWheel = {this.getWidth()/3, this.getHeight()/2 + 5, 25, 60};
-        //int[] lowerLeftWheel = {this.getWidth()/3, this.getHeight()/2 + 145, 25, 60};
+		// To get rid of ...not now
+		/*int[] chassis  = {this.getWidth()/3 + 25, this.getHeight()/2 + 5, 150, 200}; // X Y Width Height
+		int[] upperRightWheel = {this.getWidth()/3 + 175, this.getHeight()/2 + 5, 25, 60};
+		int[] lowerRightWheel = {this.getWidth()/3 + 175, this.getHeight()/2 + 145, 25, 60};
+		int[] upperLeftWheel = {this.getWidth()/3, this.getHeight()/2 + 5, 25, 60};
+		int[] lowerLeftWheel = {this.getWidth()/3, this.getHeight()/2 + 145, 25, 60};*/
 	}
 	
 	// (Graphics2D) Draw a new line at given cordinates
@@ -78,31 +81,17 @@ public class Main extends JPanel implements ActionListener {
 		}
 	}
 	
-	// (Graphics) Draw a new line at given cordinates
-	public void newLine(Graphics g, int[] points) {
-		if (points.length < 4 || points.length > 4) {
-			System.err.println("Not enough integers");
-		} else {
-			g.drawLine(points[0], points[1], points[2], points[3]);
-		}
-	}
-	
 	// (Graphics2D) Draw a new Rectangle at given cordinates with a specified width and height
-	public void newRect(Graphics2D g, int[] points) {
+	public Rectangle newRect(Graphics2D g, int[] points) {
 		if (points.length < 4 || points.length > 4) {
 			System.err.println("Not enough integers");
 		} else {
-			g.drawRect(points[0], points[1], points[2], points[3]);
+			Rectangle r = new Rectangle(points[0], points[1], points[2], points[3]);
+			g.draw(r);
+			return r;
 		}
-	}
-	
-	// (Graphics) Draw a new Rectangle at given cordinates with a specified width and height
-	public void newRect(Graphics g, int[] points) {
-		if (points.length < 4 || points.length > 4) {
-			System.err.println("Not enough integers");
-		} else {
-			g.drawRect(points[0], points[1], points[2], points[3]);
-		}
+		// Fallback solution, return something, but nothing
+		return new Rectangle(0, 0, 0, 0);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
@@ -112,19 +101,24 @@ public class Main extends JPanel implements ActionListener {
 	public static void main(String[] args) {
 		Main main = new Main();
 		JFrame frame = new JFrame("Robot Stimulator");
-		frame.setSize(frameWidth, frameHeight);
+		// Set Size
+		frame.setPreferredSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+		// Full screen
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		// Undecorated
+		frame.setUndecorated(true);
 		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(main); // You could use: frame.add(main);
+		//frame.setDefaultCloseOperation(null);
+		frame.getContentPane().add(main); // You could use: frame.add(main);
         
-        while(true) {
-            try {
-                Thread.sleep(8);
-                rotation = rotation+1;
-                frame.repaint();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+		while(true) {
+			try {
+				Thread.sleep(8);
+				rotation = rotation+1;
+				frame.repaint();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
