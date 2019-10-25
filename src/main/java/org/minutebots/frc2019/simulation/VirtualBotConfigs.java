e59d6be9f00a3410b7d178fb567cd2f514ec6b4d
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-10-25 14:13:10 -0500

    Create VirtualBotConfigs.java

---
+++ src/main/java/org/minutebots/frc2019/simulation/VirtualBotConfigs.java
@@ -0,0 +1,18 @@
+public interface VirtualBotConfigs {
+    public String getImagePath();
+    public double getX();
+    public double getY();
+    public double getAngle();
+    public void rotate(double angle);
+    public void setPosition(double x, double y);
+    public void setPosition(double inputX, double inputY, double angle);
+    public void setSpeed(int newSpeed);
+    public void setDrivetrain(String type);
+    public String getDrivetrain();
+    
+    // MISC FUNCTIONS
+    public void setTeamNumber(String teamNumber);
+    public String getTeamNumber();
+    public void setTeamName(String name);
+    public String getTeamName();
+}
