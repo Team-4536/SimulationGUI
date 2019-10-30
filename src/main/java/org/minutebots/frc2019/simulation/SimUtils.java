ab88b421e47e19e9a3c34f19d9b64d36b2579eef
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-10-29 19:29:16 -0500

    Update SimUtils.java

--- src/main/java/org/minutebots/frc2019/simulation/SimUtils.java
+++ src/main/java/org/minutebots/frc2019/simulation/SimUtils.java
@@ -1,4 +1,7 @@
+package org.minutebots.frc2019.simulation;
+
 public interface SimUtils {
     public final boolean testMode = true; // Use this to toggle certain sysrem prints
     public final boolean replitTesting = true;
+    public final boolean isSimulating = true;
 }
