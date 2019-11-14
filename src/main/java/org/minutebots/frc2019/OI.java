3b17e5d05bbf7e7439531ac1cca191682eb17143
Kevin Tran <26168327+kevintran-git@users.noreply.github.com>
2019-11-14 15:56:19 -0600

    Delete OI.java

--- src/main/java/org/minutebots/frc2019/OI.java
+++
@@ -1,8 +0,0 @@
-package org.minutebots.frc2019;
-
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-
-public class Oi {
-    public static final XboxController xController = new XboxController(0);
-}
