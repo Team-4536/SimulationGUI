0fadcce59faa8cb8136fc764b4ed13afe4091f06
ridley-nelson17 <ridley.nelson17@gmail.com>
2019-12-19 16:47:32 -0600

    Create SimUtils.java

---
+++ src/main/java/org/minutebots/frc2019/simulation/SimUtils.java
@@ -0,0 +1,5 @@
+public interface SimUtils {
+    public final boolean testMode = true; // Use this to toggle certain sysrem prints
+    public final boolean replitTesting = true;
+    public final boolean isSimulating = true;
+}
